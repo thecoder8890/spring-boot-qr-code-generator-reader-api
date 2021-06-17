93fee72da8a5dbe0959837a86316fc92dec1737a
Mayur Ingle <inglemayur8890@gmail.com>
2021-06-17 19:06:04 +0530

    commit

---
+++ src/main/java/com/onurdesk/iris/SpringBootQrCodeGeneratorReaderApplication.java
@@ -0,0 +1,13 @@
+package com.onurdesk.iris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGeneratorReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGeneratorReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurdesk/iris/SpringBootQrCodeGeneratorReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurdesk.iris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGeneratorReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
